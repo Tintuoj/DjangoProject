1d7857cfa114a5bb869e03f8cdc9fe650055af16
tintu <tintu.oj@gmail.com>
2022-03-21 15:05:12 +0000

    set up all fuctionalities

---
+++ Angularui/src/app/login-logout/logout/logout.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../auth.service';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.css']
+})
+export class LogoutComponent implements OnInit {
+
+  error: any;
+
+  constructor(private authService: AuthService,
+    private router: Router,) { }
+
+  ngOnInit(): void {
+  }
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['login']);
+    window.location.reload();
+    
+  }
+}
